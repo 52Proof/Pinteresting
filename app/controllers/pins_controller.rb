19c11820f2231828a96e7cb3f15ed13918a18f8d
Kane <mcloughlinkane1@gmail.com>
2015-03-24 18:18:50 -0400

    fixed heroku mistake

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -18,23 +18,23 @@ class PinsController < ApplicationController
   def create
     @pin = Pin.new(pin_params)
     if @pin.save
-      redirect_to @pin, notice: 'Pin was successfully created.' 
+      redirect_to @pin, notice: 'Pin was successfully created.'
     else
-      render :new 
+      render action: 'new'
     end
   end
 
   def update
     if @pin.update(pin_params)
-      redirect_to @pin, notice: 'Pin was successfully updated.' }
+      redirect_to @pin, notice: 'Pin was successfully updated.'
     else
-      render :edit 
+      render action: 'edit'
     end
   end
 
   def destroy
     @pin.destroy
-    redirect_to pins_url, notice: 'Pin was successfully destroyed.' 
+    redirect_to pins_url
   end
 
   private
@@ -45,6 +45,6 @@ class PinsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description, :string)
+      params.require(:pin).permit(:description)
     end
 end
