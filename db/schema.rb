355c86f51e1f55ddff1be3c945d1244a5d560f06
Kane <mcloughlinkane1@gmail.com>
2015-03-29 11:56:37 -0400

    fixed bugs edit/delete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150327232448) do
+ActiveRecord::Schema.define(version: 20150329151946) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20150327232448) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
